91800ba8b59cea9470fbbebe680e47bfab90aebe
STUDIES <kamil.szostek97@gmail.com>
2020-03-20 01:29:45 +0100

    done

--- Lab2/lab2_solution.py
+++ Lab2/lab2_solution.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine, MetaData, Table, select, and_
+from sqlalchemy import create_engine, MetaData, Table, select, and_, desc, Integer, func, or_
 
-#Ex 1
+
 db_string = "postgres://wbauer_adb:adb2020@pgsql-196447.vipserv.org:5432/wbauer_adb"
 
 db = create_engine(db_string)
 
+metadata = MetaData()
 
 # Ex 2
 
@@ -34,4 +35,72 @@ stmt = select([actor.c.first_name, actor.c.last_name, film.c.title, language.c.n
 results = db.execute(stmt).fetchall()
 
 # Print results
-print(results)
+for result in results:
+   print(result.first_name, " ", result.last_name, " ", result.title, " ", result.name)
+
+
+# Ex 3
+category = Table('category', metadata , autoload=True, autoload_with=db)
+
+stmt = select([func.count()]).select_from(category)
+results = db.execute(stmt).fetchall()
+
+# Print results
+print(results[0][0])
+
+
+# Ex 4
+category = Table('category', metadata , autoload=True, autoload_with=db)
+
+stmt = select([category.c.name])
+
+results = db.execute(stmt).fetchmany(size=2)
+
+# Print results
+for result in results:
+   print(result.name )
+
+
+# Ex 5
+rental = Table('rental', metadata , autoload=True, autoload_with=db)
+inventory = Table('inventory', metadata , autoload=True, autoload_with=db)
+film = Table('film', metadata , autoload=True, autoload_with=db)
+
+stmt = select([film.c.title, rental.c.rental_date]).where(and_(film.c.film_id == inventory.c.film_id,
+                                                        inventory.c.inventory_id == rental.c.inventory_id)).order_by(rental.c.rental_date)
+
+
+
+results = db.execute(stmt).fetchmany(size=1)
+
+# Print results
+for result in results:
+   print(result.title, " ", result.rental_date)
+
+
+stmt = select([film.c.title, rental.c.rental_date]).where(and_(film.c.film_id == inventory.c.film_id,
+                                                        inventory.c.inventory_id == rental.c.inventory_id)).order_by(desc(rental.c.rental_date))
+
+
+
+results = db.execute(stmt).fetchmany(size=1)
+
+# Print results
+for result in results:
+   print(result.title, " ", result.rental_date)
+
+
+# Ex 6
+actor = Table('actor', metadata , autoload=True, autoload_with=db)
+
+stmt = select([actor.c.first_name, actor.c.last_name]).where(or_(actor.c.first_name == "Olympia",
+                                                        actor.c.first_name == "Julia",
+                                                        actor.c.first_name == "Ellen"))
+#stmt = select([actor.c.first_name])
+
+
+results = db.execute(stmt).fetchall()
+
+# Print results
+for result in results:
+   print(result.first_name, " ", result.last_name)
